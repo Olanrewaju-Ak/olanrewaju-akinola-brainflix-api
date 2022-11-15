563e48be84a1593908ec7ce86cef88136bc7a6c7
Olanrewaju-Ak <akinola.lanrewaju@yahoo.com>
2022-11-14 22:20:10 -0800

    second commit, express app set up

---
+++ index.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const path = require("node: path");
+
+const app = express();
+
+app.listen(8080, () => {
+	console.log("Port 8080 is listening to express app");
+});
